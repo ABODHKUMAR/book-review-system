987f334509de692dfd8b0c0b1a48d98d2a08ad92
abodh5921 <abodh5921@gmail.com>
2025-05-20 21:56:09 +0530

    add code to connect with mongodb database

---
+++ README.md
@@ -0,0 +1,8 @@
+# Book Review System
+
+## Tech Stack
+- Node.js + Express
+- MongoDB + Mongoose
+- JSON Web Tokens for auth
+
+
--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ import cors from 'cors';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import dotenv  from 'dotenv';
-
-const app = express();
+import { connectDB } from './config/db.js';
 dotenv.config();
+const app = express();
+
 const PORT = process.env.PORT || 8080;
 
-
+connectDB().then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
